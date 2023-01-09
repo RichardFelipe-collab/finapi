d014e3528237253c4e08dc50d3c1c8a154c5fbf0
RichardFelipe-collab <richardfelipe.123@hotmail.com>
2023-01-09 16:30:56 -0300

    operação de depositos

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### Requisitos
 
 - [x] Deve ser possivel criar uma conta
-- [] Deve ser possivel buscar o extrato bancario do cliente 
+- [x] Deve ser possivel buscar o extrato bancario do cliente 
 - [] Deve ser possivel realizar um depósito
 - [] Deve ser possivel realizar um saque
 - [] Deve ser possivel buscar um extrato bacário do cliente por data
@@ -12,7 +12,7 @@
 ## Regras de negócio
 
 - [x] Não deve ser possível cadastrar uma conta com CPF já existente
-- [] Não deve ser possível fazer deposito em uma conta com CPF já existente
+- [x] Não deve ser possível fazer deposito em uma conta com CPF já existente
 - [] Não deve ser possível buscar extrato em uma contanão existente
 - [] Não deve ser possível excluir uma conta não exitente
 - [] Não deve ser possível fazer saque quando o saldo for insuficiente.
--- src/index.js
+++ src/index.js
@@ -1,43 +1,74 @@
-const { request } = require('express')
-const express= require('express')
-const { v4: uuidv4 } = require("uuid")
+const { request, response } = require("express");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-const customers = []
+const customers = [];
 
-app.post("/account", (request,response)=>{
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
+app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
-  const customerAlredyExists = customers.some((customer) => customer.cpf === cpf);
+  const customerAlredyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
 
-  if(customerAlredyExists){
-    return response.sendStatus(400).json({error:"Custumr alredy exists"})
+  if (customerAlredyExists) {
+    return response.sendStatus(400).json({ error: "Custumr alredy exists" });
   }
-  const id = uuidv4()
+  const id = uuidv4();
 
   customers.push({
     cpf,
     name,
     id: uuidv4(),
-    statement:[]
-  })
+    statement: [],
+  });
   return response.sendStatus(201).send();
-})
+});
 
-app.get("/statement", (request, response)=>{
-  const { cpf } = request.headers;
+//app.use(verifyIfExistsAccountCPF)
 
-  const customer = customers.find(customer => customer.cpf === cpf);
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
 
-  if(!customer){
-    return response.sendStatus(400).json({error:"Customer not found"});
-
-  }
+  console.log(request)
 
   return response.json(customer.statement);
-})
+});
 
-app.listen(3333)
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+  const {customer}=request;
+
+  const statemetOperation={
+    description,
+    amount,
+    created_at:new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statemetOperation);
+
+  return response.status(201).send();
+});
+
+app.listen(3333);
